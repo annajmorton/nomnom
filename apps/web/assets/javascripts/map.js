dd092ae1733f2fe6085415320ebdf618527867df
annajmorton <annajmorton@gmail.com>
2017-07-07 10:44:01 -0500

    adding google maps embed

--- apps/web/assets/javascripts/map.js
+++ apps/web/assets/javascripts/map.js
@@ -1,25 +1,23 @@
+var geocoder
+
 function initMap() {
     // Create a map object and specify the DOM element for display.
-    var latlng = {lat: -25.363, lng: 131.044};
+    var latlng = {lat: 29.481137, lng: -98.7945996};
+
     var map = new google.maps.Map(document.getElementById('map'), {
       center: latlng,
       scrollwheel: false,
       zoom: 8
     });
-  	var marker = new google.maps.Marker({
-      position: latlng,
-      map: map
-    })
 }
 
-   
+function showFood(){
 
-function codeAddress() {
-	// var address = document.getElementById('address').value;
-	var address = '14331 Hill Prince San Antonio, Tx 78248'
+    geocoder = new google.maps.Geocoder();
+	var address = document.getElementsByClassName('address').value;
+	console.log(address)
 	geocoder.geocode( { 'address': address}, function(results, status) {
 	  if (status == 'OK') {
-	    map.setCenter(results[0].geometry.location);
 	    var marker = new google.maps.Marker({
 	        map: map,
 	        position: results[0].geometry.location
